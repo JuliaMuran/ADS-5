4ecda7b909384943ce4a383b8ae9f006444a76f0
JulietM <150517302+JuliaMuran@users.noreply.github.com>
2024-04-14 20:54:20 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -3,8 +3,62 @@
 #define INCLUDE_TSTACK_H_
 
 template<typename T, int size>
-class TStack {
-  // добавьте код стека
-};
+struct TStack
+{
+private:
+	T* arr = NULL;
+	int current;
 
+public:
+	void push(T info)
+	{
+		if (size == current)
+		{
+			std::cout << "Переполнение стека" << std::endl;
+		}
+		else if (arr == NULL)
+		{
+			arr = new T[size];
+			current = 0;
+			arr[current++] = info;
+		}
+		else
+		{
+			arr[current++] = info;
+		}
+	}
+	bool isEmpty()
+	{
+		if (current == 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	T top()
+	{
+		if (isEmpty())
+		{
+			return 0;
+		}
+		else
+		{
+			return arr[current - 1];
+		}
+	}
+	T pop()
+	{
+		if (isEmpty())
+		{
+			return 0;
+		}
+		else
+		{
+			return arr[--current];
+		}
+	}
+};
 #endif  // INCLUDE_TSTACK_H_
