b5426c421b397d894ec38125a8754691e4bb92b2
JulietM <150517302+JuliaMuran@users.noreply.github.com>
2024-04-14 21:32:09 +0300

    Update alg.h

--- include/alg.h
+++ include/alg.h
@@ -13,23 +13,20 @@ std::string infx2pstfx(std::string inf) {
         for (int i = 0; i < inf.length(); i++) {
             if (inf[i] == '(') {
                 stack1.push(inf[i]);
-            }
-            else if (inf[i] == ')') {
+            } else if (inf[i] == ')') {
                 while (!stack1.isEmpty() && stack1.top() != '(') {
                     res += stack1.pop();
                 }
                 res += " ";
                 stack1.pop();
-            }
-            else if (inf[i] >= '0' && inf[i] <= '9') {
+            } else if (inf[i] >= '0' && inf[i] <= '9') {
                 while (i < inf.length() && (inf[i] >= '0' && inf[i] <= '9')) {
                     res += inf[i];
                     i++;
                 }
                 res += " ";
                 i--;
-            }
-	    else if (strchr("(+-*/^", inf[i])) {
+            } else if (strchr("(+-*/^", inf[i])) {
                 char op = inf[i];
                 while (!stack1.isEmpty()) {
                     int priorS = 0;
@@ -64,14 +61,12 @@ int eval(string post) {
     for (int i = 0; i < post.length(); i++) {
         if (post[i] >= '0' && post[i] <= '9') {
             number += post[i];
-        }
-        else if (post[i] == ' ') {
+        } else if (post[i] == ' ') {
             if (number != "") {
                 stack2.push(std::stoi(number));
                 number = "";
             }
-        }
-        else if (post[i] == '+' || post[i] == '-' || post[i] == '*' || post[i] == '/') {
+        } else if (post[i] == '+' || post[i] == '-' || post[i] == '*' || post[i] == '/') {
             switch (post[i]) {
                 case '*': {
                     stack2.push(stack2.pop() * stack2.pop());
