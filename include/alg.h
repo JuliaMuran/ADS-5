ac5ae0a0f61b9b50107e200be9ffc39a5a3b8ac6
JulietM <150517302+JuliaMuran@users.noreply.github.com>
2024-04-14 21:04:41 +0300

    Update alg.h

--- include/alg.h
+++ include/alg.h
@@ -8,63 +8,50 @@
 using namespace std;
 #include <string>
 
-std::string infx2pstfx(std::string inf)
-{
+std::string infx2pstfx(std::string inf) {
 	std::string res = "";
-	for (int i = 0; i < inf.length(); i++)
-	{
-		if (inf[i] == '(')
-		{
+	for (int i = 0; i < inf.length(); i++) {
+		if (inf[i] == '(') {
 			stack1.push(inf[i]);
 		}
-		else if (inf[i] == ')')
-		{
-			while (!stack1.isEmpty() && stack1.top() != '(')
-			{
+		else if (inf[i] == ')') {
+			while (!stack1.isEmpty() && stack1.top() != '(') {
 				res += stack1.pop();
 			}
 			res += " ";
 			stack1.pop();
 		}
-		else if (inf[i] >= '0' && inf[i] <= '9')
-		{
-			while (i < inf.length() && (inf[i] >= '0' && inf[i] <= '9'))
-			{
+		else if (inf[i] >= '0' && inf[i] <= '9') {
+			while (i < inf.length() && (inf[i] >= '0' && inf[i] <= '9')) {
 				res += inf[i];
 				i++;
 			}
 			res += " ";
 			i--;
 		}
-		else if (strchr("(+-*/^", inf[i]))
-		{
+		else if (strchr("(+-*/^", inf[i])) {
 			char op = inf[i];
-			while (!stack1.isEmpty())
-			{
+			while (!stack1.isEmpty()) {
 				int priorS = 0;
-				switch (stack1.top())
-				{
+				switch (stack1.top()) {
 				case '+': priorS = 1;
 				case '-': priorS = 1;
 				case '*': priorS = 2;
 				case '/': priorS = 2;
 				}
 				int priorOp = 0;
-				switch (op)
-				{
+				switch (op) {
 				case '+': priorOp = 1;
 				case '-': priorOp = 1;
 				case '*': priorOp = 2;
 				case '/': priorOp = 2;
 				}
-				if (!(priorS >= priorOp))
-				{
+				if (!(priorS >= priorOp)) {
 					break;
 				}
 				res = res + stack1.pop() + " ";
 			}
 			stack1.push(op);
-
 		}
 	}
 	while (!stack1.isEmpty()) {
@@ -72,19 +59,14 @@ std::string infx2pstfx(std::string inf)
 	}
 	return res;
 }
-
-int eval(string post)
-{
+int eval(string post) {
 	std::string number = "";
-	for (int i = 0; i < post.length(); i++)
-	{
+	for (int i = 0; i < post.length(); i++) {
 		if (post[i] >= '0' && post[i] <= '9') {
 			number += post[i];
 		}
 		else if (post[i] == ' ') {
-
-			if (number != "")
-			{
+			if (number != "") {
 				stack2.push(std::stoi(number));
 				number = "";
 			}
@@ -115,5 +97,4 @@ int eval(string post)
 	}
 	return stack2.pop();
 }
-
 #endif  // INCLUDE_ALG_H_
