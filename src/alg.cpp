5a04a5b4c1848be59f1df6ede4e3bea73f699a31
JulietM <150517302+JuliaMuran@users.noreply.github.com>
2024-04-19 09:26:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,11 +4,87 @@
 #include "tstack.h"
 
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+  std::string res = "";
+        for (int i = 0; i < inf.length(); i++) {
+            if (inf[i] == '(') {
+                stack1.push(inf[i]);
+            } else if (inf[i] == ')') {
+                while (!stack1.isEmpty() && stack1.top() != '(') {
+                    res += stack1.pop();
+                }
+                res += " ";
+                stack1.pop();
+            } else if (inf[i] >= '0' && inf[i] <= '9') {
+                while (i < inf.length() && (inf[i] >= '0' && inf[i] <= '9')) {
+                    res += inf[i];
+                    i++;
+                }
+                res += " ";
+                i--;
+            } else if (strchr("(+-*/^", inf[i])) {
+                char op = inf[i];
+                while (!stack1.isEmpty()) {
+                    int priorS = 0;
+                    switch (stack1.top()) {
+                        case '+': priorS = 1;
+                        case '-': priorS = 1;
+                        case '*': priorS = 2;
+                        case '/': priorS = 2;
+                    }
+                    int priorOp = 0;
+                    switch (op) {
+                        case '+': priorOp = 1;
+                        case '-': priorOp = 1;
+                        case '*': priorOp = 2;
+                        case '/': priorOp = 2;
+                    }
+                    if (!(priorS >= priorOp)) {
+                        break;
+                    }
+                    res = res + stack1.pop() + " ";
+                }
+                stack1.push(op);
+            }
+        }
+        while (!stack1.isEmpty()) {
+            res += stack1.pop();
+        }
+        return res;
+
 }
 
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+  std::string number = "";
+    for (int i = 0; i < post.length(); i++) {
+        if (post[i] >= '0' && post[i] <= '9') {
+            number += post[i];
+        } else if (post[i] == ' ') {
+            if (number != "") {
+                stack2.push(std::stoi(number));
+                number = "";
+            }
+        } else if (post[i] == '+' || post[i] == '-' || post[i] == '*' || post[i] == '/') {
+            switch (post[i]) {
+                case '*': {
+                    stack2.push(stack2.pop() * stack2.pop());
+                    break;
+                }
+                case '/': {
+                    stack2.push(stack2.pop() / stack2.pop());
+                    break;
+                }
+                case '+': {
+                    stack2.push(stack2.pop() + stack2.pop());
+                    break;
+                }
+                case '-': {
+                    int left = stack2.pop();
+                    int right = stack2.pop();
+                    stack2.push(right - left);
+                    break;
+                }
+            }
+        }
+    }
+    return stack2.pop();
 }
