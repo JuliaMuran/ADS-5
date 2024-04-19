24a08c6634e85df9c22b2b335574d83b0d3145bf
JulietM <150517302+JuliaMuran@users.noreply.github.com>
2024-04-19 09:33:03 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -6,42 +6,42 @@
 
 template<typename T, int size>
 class TStack {
-        private:
-        T* arr = NULL;
-        int current;
+private:
+    T* arr = NULL;
+    int current;
 
-        public:
-            void push(T info) {
-                if (size == current) {
-                    std::cout << "Переполнение стека" << std::endl;
-                } else if (arr == NULL) {
-                    arr = new T[size];
-                    current = 0;
-                    arr[current++] = info;
-                } else {
-                    arr[current++] = info;
-                }
-            }
-            bool isEmpty() {
-                if (current == 0) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-            T top() {
-                if (isEmpty()) {
-                    return 0;
-                } else {
-                    return arr[current - 1];
-                }
-            }
-            T pop() {
-                if (isEmpty()) {
-                    return 0;
-                } else {
-                    return arr[--current];
-                }
-            }
+public:
+    void push(T info) {
+        if (size == current) {
+            std::cout << "Переполнение стека" << std::endl;
+        } else if (arr == NULL) {
+            arr = new T[size];
+            current = 0;
+            arr[current++] = info;
+        } else {
+            arr[current++] = info;
+        }
+    }
+    bool isEmpty() {
+        if (current == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    T top() {
+        if (isEmpty()) {
+            return 0;
+        } else {
+            return arr[current - 1];
+        }
+    }
+    T pop() {
+        if (isEmpty()) {
+            return 0;
+        } else {
+            return arr[--current];
+        }
+    }
 };
 #endif  // INCLUDE_TSTACK_H_
