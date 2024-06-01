701fbba0a5bed31c747c054ef1b0b9129b6ffb59
JulietM <150517302+JuliaMuran@users.noreply.github.com>
2024-06-01 15:05:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,86 +6,83 @@
 TStack<char, 100> stack1;
 TStack<int, 100> stack2;
 
+int getPriority(char op) {
+    switch (op) {
+        case '+': case '-': return 1;
+        case '*': case '/': return 2;
+        default: return -1;
+    }
+}
+
 std::string infx2pstfx(std::string inf) {
-  std::string res = "";
-        for (int i = 0; i < inf.length(); i++) {
-            if (inf[i] == '(') {
-                stack1.push(inf[i]);
-            } else if (inf[i] == ')') {
-                while (!stack1.isEmpty() && stack1.top() != '(') {
-                    res += stack1.pop();
-                }
+    std::string res = "";
+    for (int i = 0; i < inf.length(); i++) {
+        if (inf[i] == '(') {
+            stack1.push(inf[i]);
+        } else if (inf[i] == ')') {
+            while (!stack1.isEmpty() && stack1.top() != '(') {
+                res += stack1.pop();
                 res += " ";
-                stack1.pop();
-            } else if (inf[i] >= '0' && inf[i] <= '9') {
-                while (i < inf.length() && (inf[i] >= '0' && inf[i] <= '9')) {
-                    res += inf[i];
-                    i++;
-                }
-                res += " ";
-                i--;
-            } else if (strchr("(+-*/^", inf[i])) {
-                char op = inf[i];
-                while (!stack1.isEmpty()) {
-                    int priorS = 0;
-                    switch (stack1.top()) {
-                        case '+': priorS = 1;
-                        case '-': priorS = 1;
-                        case '*': priorS = 2;
-                        case '/': priorS = 2;
-                    }
-                    int priorOp = 0;
-                    switch (op) {
-                        case '+': priorOp = 1;
-                        case '-': priorOp = 1;
-                        case '*': priorOp = 2;
-                        case '/': priorOp = 2;
-                    }
-                    if (!(priorS >= priorOp)) {
-                        break;
-                    }
-                    res = res + stack1.pop() + " ";
-                }
-                stack1.push(op);
             }
+            stack1.pop();
         }
-        while (!stack1.isEmpty()) {
-            res += stack1.pop();
+        else if (inf[i] >= '0' && inf[i] <= '9') {
+            while (i < inf.length() && (inf[i] >= '0' && inf[i] <= '9')) {
+                res += inf[i];
+                i++;
+            }
+            res += " ";
+            i--;
         }
-        return res;
+        else if (strchr("+-*/^", inf[i])) {
+            char op = inf[i];
+            while (!stack1.isEmpty() && getPriority(stack1.top()) >= getPriority(op)) {
+                res += stack1.pop();
+                res += " ";
+            }
+            stack1.push(op);
+        }
+    }
+    while (!stack1.isEmpty()) {
+        res += stack1.pop();
+        res += " ";
+    }
+    return res;
 }
 
 int eval(std::string pref) {
-  std::string number = "";
+    std::string number = "";
     for (int i = 0; i < pref.length(); i++) {
         if (pref[i] >= '0' && pref[i] <= '9') {
             number += pref[i];
-        } else if (pref[i] == ' ') {
+        }
+        else if (pref[i] == ' ') {
             if (number != "") {
                 stack2.push(std::stoi(number));
                 number = "";
             }
-        } else if (pref[i] == '+' || pref[i] == '-'
+        }
+        else if (pref[i] == '+' || pref[i] == '-'
             || pref[i] == '*' || pref[i] == '/') {
+            int right = stack2.pop();
+            int left = stack2.pop();
             switch (pref[i]) {
                 case '*': {
-                    stack2.push(stack2.pop() * stack2.pop());
-                    break;
-                }
+                        stack2.push(left * right);
+                        break;
+                    }
                 case '/': {
-                    stack2.push(stack2.pop() / stack2.pop());
-                    break;
-                }
+                        stack2.push(left / right);
+                        break;
+                    }
                 case '+': {
-                    stack2.push(stack2.pop() + stack2.pop());
-                    break;
-                }
+                        stack2.push(left + right);
+                        break;
+                    }
                 case '-': {
-                    int left = stack2.pop();
-                    int right = stack2.pop();
-                    stack2.push(right - left);
-                    break;
-                }
+                        stack2.push(left - right);
+                        break;
+                    }
             }
         }
     }
